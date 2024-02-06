2f012712216bf14a40533cf3748accebfacc176d
Vegard Falldalen <vegfall@gmail.com>
2024-02-06 20:58:43 +0100

    Added Maps from previous project

--- module/application/application.tsx
+++ module/application/application.tsx
@@ -1,5 +1,63 @@
-import React from "react";
+import React, { MutableRefObject, useEffect, useRef, useState } from "react";
+import { Map, View } from "ol";
+import TileLayer from "ol/layer/Tile";
+import { OSM } from "ol/source";
+import { useGeographic } from "ol/proj";
+import "ol/ol.css";
+import "./application.css";
+import { KommuneLayerCheckbox } from "../kommune/kommuneLayerCheckbox";
+import { MapContext } from "../map/mapContext";
+import { Layer } from "ol/layer";
+import { KommuneAside } from "../kommune/kommuneAside";
+
+useGeographic();
+
+const map = new Map({
+  view: new View({ center: [11, 60], zoom: 10 }),
+});
 
 export function Application() {
-  return <h1>Hello World</h1>;
+  const mapRef = useRef() as MutableRefObject<HTMLDivElement>;
+  const [layers, setLayers] = useState<Layer[]>([
+    new TileLayer({ source: new OSM() }),
+  ]);
+
+  useEffect(() => {
+    map.setTarget(mapRef.current);
+  }, []);
+
+  useEffect(() => {
+    map.setLayers(layers);
+  }, [layers]);
+
+  function handleFocusUser(e: React.MouseEvent) {
+    e.preventDefault();
+
+    navigator.geolocation.getCurrentPosition((position) => {
+      const { latitude, longitude } = position.coords;
+
+      map.getView().animate({
+        center: [longitude, latitude],
+        zoom: 12,
+      });
+    });
+  }
+
+  return (
+    <MapContext.Provider value={{ layers, setLayers }}>
+      <header>
+        <h1>My Awesome Map!</h1>
+      </header>
+      <nav>
+        <a href="#" onClick={handleFocusUser}>
+          My location
+        </a>
+        <KommuneLayerCheckbox />
+      </nav>
+      <main>
+        <div ref={mapRef}></div>
+        <KommuneAside />
+      </main>
+    </MapContext.Provider>
+  );
 }
