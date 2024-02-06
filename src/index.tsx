275e29e0b47c1a52ad18b0184a003147d958cb84
Vegard Falldalen <vegfall@gmail.com>
2024-02-06 21:14:27 +0100

    Shows and hides kommune aside

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Application } from "./module/application/application";
+import { Application } from "./modules/application/application";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 
